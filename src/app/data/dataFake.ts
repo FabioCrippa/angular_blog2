6f98d46d0a4de5d2e3052e0a5114747e9fc2bf37
Fabio Crippa <crippa.fabiocrippa@gmail.com>
2024-08-20 17:22:17 -0300

    content crerated

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,8 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "saiu novo filme",
+        "description": "ola",
+        "photoContentCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScMBPxe-IY6nyXhm1_LWzOklFy3sGKCtExZg&s"
+    },
+]
---
+++ src/app/pages/content/content.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { dataFake } from '../../data/dataFake';
+
+@Component({
+  selector: 'app-content',
+  standalone: true,
+  imports: [],
+  templateUrl: './content.component.html',
+  styleUrl: './content.component.css'
+})
+export class ContentComponent implements OnInit {
+  
+  photoContentCover:string = "";
+  contentTitle:string = "";
+  contentDescription:string = "";
+  private id:string | null = "0"
+
+  constructor(
+    private route:ActivatedRoute
+  ) {  }
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(value =>
+      //console.log(value.get("id"))
+      this.id = value.get("id")
+    )
+    this.setValuesToComponent(this.id)
+
+  }
+
+  setValuesToComponent(id:string | null) {
+    const result = dataFake.filter(article => article.id == id)[0]
+
+    this.contentTitle = result.title;
+    this.contentDescription = result.description;
+    this.photoContentCover = result.photoContentCover;
+  }
+
+}
